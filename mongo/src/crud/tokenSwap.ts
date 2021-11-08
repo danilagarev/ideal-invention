c198f277c613d7a3467da724970e6a4a624fa57a
daniil <daniil.agarev@convexitydmcc.com>
2021-11-08 14:37:27 +0400

    Add pubkey to tokenSwap and tokenAccounts

--- mongo/src/crud/tokenSwap.ts
+++ mongo/src/crud/tokenSwap.ts
@@ -3,17 +3,34 @@ import {
   TokenSwapReserveModel
 } from "../models";
 import {Numberu64, TokenSwapLayout} from "@solana/spl-token-swap"
-import {AccountInfo, PublicKey} from "@solana/web3.js"
+import {AccountInfo, ParsedAccountData, PublicKey} from "@solana/web3.js"
 import {connection} from "../../../metaplex/src/constants";
 import {dbSwitcher} from "../switcher_utils";
+import {Buffer} from "buffer";
 
+class TokenAccount {
+  account: AccountInfo<Buffer | ParsedAccountData>;
+  pubkey: string;
+  constructor(account: any, // TODO: fix type later
+              pubkey: string) {
+    this.account = account;
+    this.pubkey = pubkey;
+  }
+}
 
-async function prepareTokenSwap(tokenSwapData: any, programID: string) {
+async function prepareTokenSwap(tokenSwapData: any,
+                                programID: string,
+                                pubkey: string) {
   const tokenPool = new PublicKey(tokenSwapData.tokenPool).toBase58();
   const tokenProgramId = new PublicKey(tokenSwapData.tokenProgramId).toBase58();
   const feeAccount = new PublicKey(tokenSwapData.feeAccount).toBase58();
-  const tokenAccountA = await connection.getParsedAccountInfo(new PublicKey(tokenSwapData.tokenAccountA));
-  const tokenAccountB = await connection.getParsedAccountInfo(new PublicKey(tokenSwapData.tokenAccountB));
+  const tokenAccountA = new TokenAccount(
+    await connection.getParsedAccountInfo(new PublicKey(tokenSwapData.tokenAccountA)),
+    tokenSwapData.tokenAccountA);
+  const tokenAccountB = new TokenAccount(
+    await connection.getParsedAccountInfo(new PublicKey(tokenSwapData.tokenAccountB)),
+    tokenSwapData.tokenAccountB
+    );
   const mintA = new PublicKey(tokenSwapData.mintA).toBase58();
   const mintB = new PublicKey(tokenSwapData.mintB).toBase58();
 
@@ -48,6 +65,7 @@ async function prepareTokenSwap(tokenSwapData: any, programID: string) {
   const curveParameters = tokenSwapData.curveParameters;
 
   return {
+    pubkey,
     version,
     isInitialized,
     nonce,
@@ -76,7 +94,11 @@ export async function saveAllTokenSwap(accounts: Array<{
   account: AccountInfo<Buffer>,
   pubkey: PublicKey
 }>, programID: string): Promise<void> {
-  const promises = accounts.map(item => prepareTokenSwap(TokenSwapLayout.decode(Buffer.from(item.account.data)), programID));
+  const promises = accounts.map(item => prepareTokenSwap(
+    TokenSwapLayout.decode(Buffer.from(item.account.data)),
+    programID,
+    item.pubkey.toBase58()
+    ));
   const tokenSwap = await Promise.all(promises);
   const switcher = await dbSwitcher.getMainTable('tokenSwap');
   console.log("switcher", switcher)
