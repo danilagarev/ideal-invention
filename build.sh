42886b1b19f304e5d3993ad35b99d854660702d5
daniil <daniil.agarev@convexitydmcc.com>
2021-11-08 12:43:29 +0400

    Fix build  script

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Compile TypeScript
-echo "Compiling TypeScript..."
-tsc
-
 # Build and deploy Reader function along with API Gateway endpoint using AWS SAM CLI
 echo "Building Service..."
 docker-compose build
